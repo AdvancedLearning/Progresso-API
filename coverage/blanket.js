7a63e6ee7364bd7cd065a3b234ecf8cdaa7ac743
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2014-10-31 13:33:06 +0000

    Missing coverage configuration

--- coverage/blanket.js
+++
@@ -1,9 +0,0 @@
-/* jslint node: true */
-
-var path = require('path');
-var libDir = path.join(__dirname, '..', 'lib');
-
-require('blanket')({
-  // Only files that match the pattern will be instrumented
-  pattern: [libDir]
-});
